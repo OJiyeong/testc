fcf8c37c4ed20954e6de1f03b1af548f8f8145cc
OJiyeong <102336876+OJiyeong@users.noreply.github.com>
2022-03-25 16:10:50 +0900

    Update 3주차 과제.c

--- 3주차 과제.c
+++ 3주차 과제.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 main() {
-	/*5/(2*3) + 6/(3*4) + 7/(4*5) + ... + 13/(10*11) °á°ú°ª 3.037276 */
-/*
-	float i; float sum = 0;
-	for (i = 5; i <= 13; i++)
-		sum = sum + i / ((i-3) * (i-2));
-	printf("%f\n", sum);
-	printf("\n");
-*/	
 	int i, j, k; float sum = 0;
 	for (i = 5; i <= 13; i++) {
 		j = i - 3;
